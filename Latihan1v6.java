c8f4cac8a911fe9e3f81433c3f53cb361321b273
irfanefc11 <56101485+irfanefc11@users.noreply.github.com>
2019-10-03 11:02:34 +0700

    Java Latihan

---
+++ Latihan1v6.java
@@ -0,0 +1,20 @@
+public class Latihan1v6{
+
+public static void main(String[] args){
+
+System.out.println("hello guys");
+System.out.println("Nama : Irfan NoorFadillah Yasri");
+
+System.out.print("NPM : 1173013");
+System.out.print("KELAS : D3 TI 3A");
+
+System.out.print("Tempat Lahir : Bogor");
+System.out.println("Tanggal Lahir : 1 November 1999");
+
+System.out.print("Hoby : Tidur \n");
+System.out.print("Makan : Omnivora \n");
+System.out.print("Motto Hidup : Jangan lupa tidur \n");
+
+System.out.printf("Jangan Melawan Orang Tua %d",1173013);
+}
+}
---
+++ Latihan1v8.java
@@ -0,0 +1,20 @@
+public class Latihan1v8 {
+
+public static void main(String[] args){
+
+// kita akan membuat variabel
+// tipe data
+int a = 10; // assignment
+System.out.println("nilai a = " + a);
+
+a = 20;
+System.out.println("nilai a baru = " + a);
+
+// kita membuat sebuah deklarasi
+int b; // deklarasi
+
+b = 7;
+System.out.println("nilai b = " + b);
+
+}
+}
